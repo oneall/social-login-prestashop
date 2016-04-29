51eeb887ad12f6c00a6cdbe91e3879fe5159f3b8
ZiZuu.com <ZiZuu-store@users.noreply.github.com>
2016-04-29 16:50:53 +0200

    don't reinvent the wheel

--- PrestaShop 1.6.x.x/oneallsociallogin/includes/tools.php
+++ PrestaShop 1.6.x.x/oneallsociallogin/includes/tools.php
@@ -334,19 +334,15 @@ class oneall_social_login_tools
 		$mail_vars['{email}'] = $customer->email;
 
 		//Read the first employe - should be the board owner
-		$sql = "SELECT `firstname`, `lastname`, `email` FROM `" . _DB_PREFIX_ . "employee` WHERE id_profile="._PS_ADMIN_PROFILE_;
-		$employees = Db::getInstance ()->ExecuteS ($sql);
-		if (is_array ($employees) AND count ($employees) > 0)
+		$employees = Employee::getEmployeesByProfile(_PS_ADMIN_PROFILE_, true);
+		foreach ($employees as $employee)
 		{
-			foreach ($employees as $employee)
-			{
-				//Employee Details
-				$mail_vars['{firstname}'] = $employee['firstname'];
-				$mail_vars['{lastname}'] = $employee['lastname'];
+			//Employee Details
+			$mail_vars['{firstname}'] = $employee['firstname'];
+			$mail_vars['{lastname}'] = $employee['lastname'];
 
-				//Send Mail
-				@Mail::Send ($language_id, 'contact', $mail_title ,$mail_vars, $employee['email'], $employee['firstname'].' '.$employee['lastname']);
-			}
+			//Send Mail
+			@Mail::Send ($language_id, 'contact', $mail_title ,$mail_vars, $employee['email'], $employee['firstname'].' '.$employee['lastname']);
 		}
 
 		//Done
